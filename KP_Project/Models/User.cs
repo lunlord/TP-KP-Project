f4724c7c68b588fd83efcc665204a6785b0183fd
lunlord <vana_lalalala@mail.ru>
2022-01-13 21:37:37 +0300

    project created

---
+++ KP_Project/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KP_Project.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public int Login { get; set; }
+        public int Password { get; set; }
+        public int Phone_number { get; set; }
+        public int Adress { get; set; }
+        public int Email { get; set; }
+    }
+}
